28d8b5e533bd9731953b4cc26e1a74167ce0adcc
Ivaniv Anton <korzrinkayablok@gmail.com>
2022-09-29 17:36:05 +0300

    issue #7: - Imports refactor;

--- app/Providers/DependenciesServiceProvider.php
+++ app/Providers/DependenciesServiceProvider.php
@@ -2,41 +2,20 @@
 
 namespace App\Providers;
 
-use App\Adapters\Presenters;
-use App\Domain\Interfaces\Factories\EventFactoryInterface;
-use App\Domain\Interfaces\Factories\GameFactoryInterface;
-use App\Domain\Interfaces\Factories\PlaceFactoryInterface;
-use App\Domain\Interfaces\Factories\PlayerFactoryInterface;
-use App\Domain\Interfaces\Factories\TableFactoryInterface;
-use App\Domain\Interfaces\Repositories\EventRepositoryInterface;
-use App\Domain\Interfaces\Repositories\GameRepositoryInterface;
-use App\Domain\Interfaces\Repositories\PlaceRepositoryInterface;
-use App\Domain\Interfaces\Repositories\PlayerRepositoryInterface;
-use App\Domain\Interfaces\Repositories\TableRepositoryInterface;
-use App\Domain\Interfaces\Services\AuthServiceInterface;
-use App\Domain\Interfaces\Services\TableServiceInterface;
-use App\Domain\Interfaces\Services\TelegramServiceInterface;
+use App\Domain\Interfaces\Factories as FactoriesInterfaces;
+use App\Domain\Interfaces\Repositories as RepositoriesInterfaces;
+use App\Domain\Interfaces\Services as ServicesInterfaces;
 use App\Domain\UseCases;
-use App\Factories\EventFactory;
-use App\Factories\GameFactory;
-use App\Factories\PlaceFactory;
-use App\Factories\PlayerFactory;
-use App\Factories\TableFactory;
-use App\Http\Player\Controllers\Game\AddGameController;
-use App\Http\Player\Controllers\Login\TelegramLoginController;
-use App\Http\Player\Controllers\Table\AddTableController;
-use App\Http\Player\Controllers\Table\DeleteTableController;
-use App\Http\Player\Controllers\Table\LeaveTableController;
-use App\Http\Player\Controllers\Table\SeatAtTableController;
-use App\Http\Player\Controllers\Table\UpdateTableController;
-use App\Repositories\EventEloquentRepository;
-use App\Repositories\GameEloquentRepository;
-use App\Repositories\PlaceEloquentRepository;
-use App\Repositories\PlayerEloquentRepository;
-use App\Repositories\TableEloquentRepository;
-use App\Services\LaravelAuthService;
-use App\Services\TableService;
-use App\Services\TelegramService;
+
+use App\Adapters\Presenters;
+use App\Factories;
+use App\Http\Player\Controllers\Game;
+use App\Http\Player\Controllers\Login;
+use App\Http\Player\Controllers\Table;
+
+use App\Repositories;
+use App\Services;
+
 use Illuminate\Support\ServiceProvider;
 
 class DependenciesServiceProvider extends ServiceProvider
@@ -58,34 +37,34 @@ class DependenciesServiceProvider extends ServiceProvider
 
     private function repositories()
     {
-        $this->app->bind(EventRepositoryInterface::class, EventEloquentRepository::class);
-        $this->app->bind(GameRepositoryInterface::class, GameEloquentRepository::class);
-        $this->app->bind(TableRepositoryInterface::class, TableEloquentRepository::class);
-        $this->app->bind(PlayerRepositoryInterface::class, PlayerEloquentRepository::class);
-        $this->app->bind(PlaceRepositoryInterface::class, PlaceEloquentRepository::class);
+        $this->app->bind(RepositoriesInterfaces\EventRepositoryInterface::class, Repositories\EventEloquentRepository::class);
+        $this->app->bind(RepositoriesInterfaces\GameRepositoryInterface::class, Repositories\GameEloquentRepository::class);
+        $this->app->bind(RepositoriesInterfaces\TableRepositoryInterface::class, Repositories\TableEloquentRepository::class);
+        $this->app->bind(RepositoriesInterfaces\PlayerRepositoryInterface::class, Repositories\PlayerEloquentRepository::class);
+        $this->app->bind(RepositoriesInterfaces\PlaceRepositoryInterface::class, Repositories\PlaceEloquentRepository::class);
     }
 
     private function factories()
     {
-        $this->app->bind(EventFactoryInterface::class, EventFactory::class);
-        $this->app->bind(GameFactoryInterface::class, GameFactory::class);
-        $this->app->bind(PlaceFactoryInterface::class, PlaceFactory::class);
-        $this->app->bind(PlayerFactoryInterface::class, PlayerFactory::class);
-        $this->app->bind(TableFactoryInterface::class, TableFactory::class);
+        $this->app->bind(FactoriesInterfaces\EventFactoryInterface::class, Factories\EventFactory::class);
+        $this->app->bind(FactoriesInterfaces\GameFactoryInterface::class, Factories\GameFactory::class);
+        $this->app->bind(FactoriesInterfaces\PlaceFactoryInterface::class, Factories\PlaceFactory::class);
+        $this->app->bind(FactoriesInterfaces\PlayerFactoryInterface::class, Factories\PlayerFactory::class);
+        $this->app->bind(FactoriesInterfaces\TableFactoryInterface::class, Factories\TableFactory::class);
     }
 
     private function services()
     {
-        $this->app->bind(AuthServiceInterface::class, LaravelAuthService::class);
-        $this->app->bind(TelegramServiceInterface::class, TelegramService::class);
-        $this->app->bind(TableServiceInterface::class, TableService::class);
+        $this->app->bind(ServicesInterfaces\AuthServiceInterface::class, Services\LaravelAuthService::class);
+        $this->app->bind(ServicesInterfaces\TelegramServiceInterface::class, Services\TelegramService::class);
+        $this->app->bind(ServicesInterfaces\TableServiceInterface::class, Services\TableService::class);
 
     }
 
     private function playerControllers()
     {
         $this->app
-            ->when(AddGameController::class)
+            ->when(Game\AddGameController::class)
             ->needs(UseCases\Game\Add\InputPortInterface::class)
             ->give(function ($app) {
                 return $app->make(UseCases\Game\Add\Interactor::class, [
@@ -94,7 +73,7 @@ class DependenciesServiceProvider extends ServiceProvider
             });
 
         $this->app
-            ->when(DeleteTableController::class)
+            ->when(Table\DeleteTableController::class)
             ->needs(UseCases\Table\Delete\InputPortInterface::class)
             ->give(function ($app) {
                 return $app->make(UseCases\Table\Delete\Interactor::class, [
@@ -103,7 +82,7 @@ class DependenciesServiceProvider extends ServiceProvider
             });
 
         $this->app
-            ->when(LeaveTableController::class)
+            ->when(Table\LeaveTableController::class)
             ->needs(UseCases\Table\RemovePlayer\InputPortInterface::class)
             ->give(function ($app) {
                 return $app->make(UseCases\Table\RemovePlayer\Interactor::class, [
@@ -112,7 +91,7 @@ class DependenciesServiceProvider extends ServiceProvider
             });
 
         $this->app
-            ->when(SeatAtTableController::class)
+            ->when(Table\SeatAtTableController::class)
             ->needs(UseCases\Table\AddPlayer\InputPortInterface::class)
             ->give(function ($app) {
                 return $app->make(UseCases\Table\AddPlayer\Interactor::class, [
@@ -121,7 +100,7 @@ class DependenciesServiceProvider extends ServiceProvider
             });
 
         $this->app
-            ->when(UpdateTableController::class)
+            ->when(Table\UpdateTableController::class)
             ->needs(UseCases\Table\Update\InputPortInterface::class)
             ->give(function ($app) {
                 return $app->make(UseCases\Table\Update\Interactor::class, [
@@ -130,7 +109,7 @@ class DependenciesServiceProvider extends ServiceProvider
             });
 
         $this->app
-            ->when(AddTableController::class)
+            ->when(Table\AddTableController::class)
             ->needs(UseCases\Table\Add\InputPortInterface::class)
             ->give(function ($app) {
                 return $app->make(UseCases\Table\Add\Interactor::class, [
@@ -139,7 +118,7 @@ class DependenciesServiceProvider extends ServiceProvider
             });
 
         $this->app
-            ->when(TelegramLoginController::class)
+            ->when(Login\TelegramLoginController::class)
             ->needs(UseCases\Player\LoginViaTelegram\InputPortInterface::class)
             ->give(function ($app) {
                 return $app->make(UseCases\Player\LoginViaTelegram\Interactor::class, [
